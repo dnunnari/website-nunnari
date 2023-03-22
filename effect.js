550d49e9b31a23bc5f1d608a0d3f37c4f9e2cd7d
dnunnari <127631471+dnunnari@users.noreply.github.com>
2023-03-21 20:11:33 -0600

    First

---
+++ effect.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#typed', {
+  strings: ['Visual Designer', 'Computer Engineer', 'From Costa Rica'],
+  typeSpeed: 60,
+  backDelay: 2550,
+  loop: true,
+  loopCount: Infinity,
+});
